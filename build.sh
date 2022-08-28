b2c47d279f39fcadfbe5fa64ac417aaba30823db
Aayush Bisen <aayushbisen@protonmail.com>
2022-08-29 01:47:54 +0530

    fix superuser command

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip3 install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-django-admin createsuperuser --noinput
+python manage.py createsuperuser --noinput
